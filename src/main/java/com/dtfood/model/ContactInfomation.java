e4e29333655a7aa1e1b5498a48d25ad86f9ea34d
Tài Nguyễn Tô Đức <nguyentoductai@gmail.com>
2024-08-12 16:54:28 +0700

    first commit

---
+++ src/main/java/com/dtfood/model/ContactInfomation.java
@@ -0,0 +1,12 @@
+package com.dtfood.model;
+
+
+import lombok.Data;
+
+@Data
+public class ContactInfomation {
+    private String email;
+    private String mobile;
+    private String twitter;
+    private String instagram;
+}
---
+++ src/test/java/com/dtfood/OnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
