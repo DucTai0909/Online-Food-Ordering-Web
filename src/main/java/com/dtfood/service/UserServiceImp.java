63bee072a757ce9113a0851e91297a8ed330ae8f
tai <nguyentoductai@gmail.com>
2024-08-14 17:12:10 +0700

    singIn & signUp method

---
+++ src/main/java/com/dtfood/service/UserServiceImp.java
@@ -0,0 +1,36 @@
+package com.dtfood.service;
+
+import com.dtfood.config.JwtProvider;
+import com.dtfood.model.User;
+import com.dtfood.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImp implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Override
+    public User findUserByJwtToken(String jwt) throws Exception {
+        String email = jwtProvider.getEmailFromJwtToken(jwt);
+
+        User user = findUserByEmail(email);
+
+        return user;
+    }
+
+    @Override
+    public User findUserByEmail(String email) throws Exception {
+        User user = userRepository.findByEmail(email);
+
+        if(user == null){
+            throw new Exception("User not found with email: " + email);
+        }
+        return user;
+    }
+}
